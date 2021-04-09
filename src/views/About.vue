8013df7792a2376708fa80b81e4a4eb1715cd1b5
tutaru99 <simas.raudonis@gmail.com>
2021-04-09 13:09:08 +0300

    ci cd cypress final test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,3 @@
 <template>
-  <div>
-    <h1>left todo for this project</h1>
 
-
-    <h1>//Setup CI/CD - also deploy to web.</h1>
-
-
-  <h1> to run cypress -  ./node_modules/.bin/cypress open or npm run cypress-test</h1>
-  </div>
 </template>
-
-<style scoped>
-h1{
-  color: white;
-}
-</style>
