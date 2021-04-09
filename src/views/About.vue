27e0f7fe934d03d71d73003a0bcc2a77a2ef4113
tutaru99 <simas.raudonis@gmail.com>
2021-04-09 13:15:10 +0300

    ci cd cypress final test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,7 @@
 <template>
-
+  <div>
+    <h1>left todo for this project</h1>
+    <h1>//Setup CI/CD - also deploy to web.</h1>
+  <h1> to run cypress -  ./node_modules/.bin/cypress open or npm run cypress-test</h1>
+  </div>
 </template>
