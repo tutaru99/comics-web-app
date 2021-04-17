123212ad541ed8df24d526e7d9aa9759dc0cf9b8
tutaru99 <simas.raudonis@gmail.com>
2021-04-17 14:17:50 +0300

    ci working

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>left todo for this project</h1>
     <h1>//Setup CI/CD - also deploy to web.</h1>
-  <h1> to run cypress ---  ./node_modules/.bin/cypress open or npm run cypress-test</h1>
+  <h1> to run cypress ---  ./node_modules/.bin/cypress open or npm run cypress-test</h1>ss
   </div>
 </template>
