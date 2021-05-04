d4a902d7946e862ee2293da75b39456351e2f4c1
tutaru99 <simas.raudonis@gmail.com>
2021-05-04 13:23:21 +0300

    Testing devOps

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>left todo for this project</h1>
     <h1>//Setup CI/CD - also deploy to web.</h1>
-  <h1> to run cypress ---  ./node_modules/.bin/cypress open or npm run cypress-test</h1> ci final test
+  <h1> to run cypress ---  ./node_modules/.bin/cypress open or npm run cypress-test</h1>
   </div>
 </template>
